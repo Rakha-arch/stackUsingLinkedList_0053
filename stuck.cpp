33926361faec21de84cfc2231e5643b34c2fa693
Rakha-arch <miftahuzahran@gmail.com>
2026-05-14 15:22:06 +0700

    fungsi push

--- stuck.cpp
+++ stuck.cpp
@@ -18,3 +18,12 @@ private:
 public:
     stack() {
         top = NULL;
+    }
+    int push(int value) {
+            Node *newNode = new Node();
+            newNode->data = value;
+            newNode->next = top;
+            top = newNode;
+            cout << "Push value: " << value << endl;
+            return value;
+        }
