60bac9757b8fcf72eed9aeee96dfb132add57cdd
Rakha-arch <miftahuzahran@gmail.com>
2026-05-14 15:35:27 +0700

    supaya bisa break

--- stuck.cpp
+++ stuck.cpp
@@ -72,5 +72,7 @@ int main() {
             case 1:
                 cout << "Enter the value to push: ";
                 cin >> value;
+                stack.push(value);
+                break;
         }
 }
